08bd349e1583a9002570c34328cf64cece9d6c19
sumgr0 <sumit.grover@gmail.com>
2022-07-12 10:53:41 +0530

    Update gau-expose.sh

--- gau-expose.sh
+++ gau-expose.sh
@@ -9,57 +9,52 @@ echo -e '\033[1;38;5;221m'"
 Author:Tamim Hasan(tamimhasan404)"
 echo
 
-mkdir gau-expose-result
+path=$1
+target=$2
 
-echo -n "[] Give your gau file name/path: "
+[ ! -f ~/tools/gau-expose/gau-expose-result/$2 ] && mkdir -p ~/tools/gau-expose/gau-expose-result/$2
 
-read path
-
-echo
-
-echo "[] Greather senstive file"
-
-echo
+BASE='/root/tools/gau-expose/gau-expose-result/$2'
 
 cp $path gau-expose-result
 cd gau-expose-result
 
-cat $path | grep ".xls\|.xlsx\|.sql\|.csv\|.env\|.msql\|.bak\|.bkp\|.bkf\|.old\|.temp\|.db\|.mdb\|.config\|.yaml\|.zip\|.tar\|.git\|.xz\|.asmx\|.vcf\|.pem" | uro | sort | uniq  > gau-sensitive-file.txt
+cat $path | grep ".xls\|.xlsx\|.sql\|.csv\|.env\|.msql\|.bak\|.bkp\|.bkf\|.old\|.temp\|.db\|.mdb\|.config\|.yaml\|.zip\|.tar\|.git\|.xz\|.asmx\|.vcf\|.pem" | uro | sort | uniq  > $BASE/gau-sensitive-file.txt
 
 echo "[] Greather all panel stuff"
 
-cat $path | grep -i "login\|singup\|admin\|dashboard\|wp-admin\|singin\|adminer\|dana-na\|login/?next/=" | sort | uniq | uro > gau-panel.txt
+cat $path | grep -i "login\|singup\|admin\|dashboard\|wp-admin\|singin\|adminer\|dana-na\|login/?next/=" | sort | uniq | uro > $BASE/gau-panel.txt
 echo
 echo "[] Greather third-party assets"
 
-cat $path | grep -i "jira\|jenkins\|grafana\|mailman\|+CSCOE+\|+CSCOT+\|+CSCOCA+\|symfony\|debug\|gitlab\|phpmyadmin\|phpMyAdmin" | sort | uniq | uro > gau-third-party-assets.txt
+cat $path | grep -i "jira\|jenkins\|grafana\|mailman\|+CSCOE+\|+CSCOT+\|+CSCOCA+\|symfony\|debug\|gitlab\|phpmyadmin\|phpMyAdmin" | sort | uniq | uro > $BASE/gau-third-party-assets.txt
 
 echo
 echo "[] Greathering emails-usersnames" 
-cat $path | grep "@" | sort | uniq | uro > gau-emails-usersnames.txt
+cat $path | grep "@" | sort | uniq | uro > $BASE/gau-emails-usersnames.txt
 
 echo
 echo "[] Greathering error(may sensitive-data-expose)"
-cat $path | grep "error." | sort | uniq | uro > gau-error-base.txt
+cat $path | grep "error." | sort | uniq | uro > $BASE/gau-error-base.txt
 
 echo
 echo "[] Grathering other sensitive path"
-cat $path | grep -i "root\| internal\| private\|secret" | sort | uniq | uro > other-possible-sensitive-path.txt
+cat $path | grep -i "root\| internal\| private\|secret" | sort | uniq | uro > $BASE/other-possible-sensitive-path.txt
 
 echo
 echo "[] Grathering only robots.txt"
-cat $path | grep -i robots.txt | sort | uniq | uro > only-robots.txt
+cat $path | grep -i robots.txt | sort | uniq | uro > $BASE/only-robots.txt
 
 echo
 echo "[] Grathering subdomains"
-cat $path | cut -d'/' -f3 | cut -d':' -f1 | uro | sed 's/^\(\|s\):\/\///g' > subdomains.txt
+cat $path | cut -d'/' -f3 | cut -d':' -f1 | uro | sed 's/^\(\|s\):\/\///g' > $BASE/subdomains.txt
 
 echo
 echo "[] Grathering paths for directory brute-force"
 
-cat $path | rev | cut -d '/' -f 1 | rev | uro | sed 's/^\(\|s\):\/\///g' | sed '/=\|.js\|.gif\|.html\|.rss\|.cfm\|.htm\|.jpg\|.mp4\|.css\|.jpeg\|.png\|:\|%/d' > wordlist.txt
+cat $path | rev | cut -d '/' -f 1 | rev | uro | sed 's/^\(\|s\):\/\///g' | sed '/=\|.js\|.gif\|.html\|.rss\|.cfm\|.htm\|.jpg\|.mp4\|.css\|.jpeg\|.png\|:\|%/d' > $BASEwordlist.txt
 
-rm $path
+#rm $path
 
 echo
 
